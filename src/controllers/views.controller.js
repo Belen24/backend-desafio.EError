675d0042b2437add7397903664d6b07670fff72f
Belen <bgutierrez.mil@gmail.com>
2023-07-26 23:41:31 -0400

    customError

---
+++ src/controllers/views.controller.js
@@ -0,0 +1,22 @@
+import { ProductsService } from "../services/products.service.js";
+
+export class ViewsController{
+    static renderHome = (req,res)=>{
+        res.render("home");
+    };
+
+    static renderLogin = (req,res)=>{
+        res.render("login");
+    };
+
+    static renderSignup = (req,res)=>{
+        res.render("signup");
+    };
+
+    static renderProfile = (req,res)=>{
+        // console.log(req.user);
+        res.render("profile",{user:req.user});
+    };
+
+    
+}
